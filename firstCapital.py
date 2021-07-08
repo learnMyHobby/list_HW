0fb5c13225e3d84f28f2d7c142de2d571dd66ac4
Bibek <ilovecoding643@gmail.com>
2021-07-08 23:03:20 +0545

    Adding some changes

---
+++ firstCapital.py
@@ -0,0 +1,9 @@
+#  Change the first alphabet of all elements of the following list to capital.
+#   Bob = [‘hurricane’,’tambourine’,’blowing’,’numb’]
+
+Bob = ['hurricane','tambourine','blowing','numb']
+
+Bob = [listing.capitalize() for listing in Bob]
+
+print(Bob)
+
